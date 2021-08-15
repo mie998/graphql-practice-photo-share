87347a150a2f5821c1da0eafb25e8ea713cccc06
mie998 <nishiwaki.kyoto@gmail.com>
2021-08-16 00:57:01 +0900

    allPhotos バグったなうw

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require("apollo-server");
+const { GraphQLScalarType } = require("graphql");
 const { readFileSync } = require("fs");
 const { users, photos, tags } = require("./sample_data");
 
@@ -9,13 +10,16 @@ let _id = 0;
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
-    allPhotos: () => photos,
+    allPhotos: (parent, args) => {
+      photos.filter((p) => new Date(p.created) >= new Date(args.after));
+    },
   },
   Mutation: {
     postPhoto(parent, args) {
       let newPhoto = {
         id: _id++,
         ...args.input,
+        created: new Date(),
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -27,17 +31,24 @@ const resolvers = {
       return tags
         .filter((t) => t.photoID === parent.id)
         .map((t) => t.userID)
-        .filter((uid) => users.filter((u) => u.githubLogin === uid));
+        .map((uid) => users.find((u) => u.githubLogin === uid));
     },
   },
   User: {
     inPhotos: (parent) => {
       tags
-        .filter((tag) => tag.userID === parent.userID)
+        .filter((tag) => tag.userID === parent.id)
         .map((tag) => tag.photoID)
-        .filter((pid) => photos.filter((p) => p.githubLogin === pid));
+        .map((pid) => photos.find((p) => p.githubLogin === pid));
     },
   },
+  DateTime: new GraphQLScalarType({
+    name: `DateTime`,
+    description: `A valid date time value.`,
+    parseValue: (value) => new Date(value),
+    serialize: (value) => new Date(value).toISOString(),
+    parseLiteral: (ast) => ast.value,
+  }),
 };
 
 const server = new ApolloServer({
--- sample_data.js
+++ sample_data.js
@@ -6,10 +6,34 @@ exports.users = [
 ];
 
 exports.photos = [
-  { id: "1", name: "sample1", category: "SELFIE", githubLogin: "mie998" },
-  { id: "2", name: "sample2", category: "LANDSCAPE", githubLogin: "mie99" },
-  { id: "3", name: "sample3", category: "ACTION", githubLogin: "mie98" },
-  { id: "4", name: "sample5", category: "SELFIE", githubLogin: "mie8" },
+  {
+    id: "1",
+    name: "sample1",
+    category: "SELFIE",
+    githubLogin: "mie998",
+    created: "4/18/2019",
+  },
+  {
+    id: "2",
+    name: "sample2",
+    category: "LANDSCAPE",
+    githubLogin: "mie99",
+    created: "4/18/2029",
+  },
+  {
+    id: "3",
+    name: "sample3",
+    category: "ACTION",
+    githubLogin: "mie98",
+    created: "4/18/2029",
+  },
+  {
+    id: "4",
+    name: "sample5",
+    category: "SELFIE",
+    githubLogin: "mie8",
+    created: "4/18/2099",
+  },
 ];
 
 exports.tags = [
